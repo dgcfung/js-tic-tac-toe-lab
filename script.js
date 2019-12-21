ecbb0481594592c3a16bfb4a8585991cab893b7e
B Legit <fungd@Donalds-MacBook-Air.local>
2019-12-21 11:08:34 -0500

    initial commit, header, reset button

---
+++ script.js
@@ -0,0 +1,13 @@
+// Reset button;
+
+let resetButton= document.querySelector('reset-button')
+console.log(resetButton)
+
+resetButton.addEventListener('click', resetGame)
+
+function resetGame(){
+    // resets game, run script again
+}
+
+
+
